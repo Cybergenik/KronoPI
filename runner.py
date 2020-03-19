e380165a0822d99917cbe4931c14a21ff8d7421c
Luciano Remes <cybergenik@gmail.com>
2020-03-19 13:12:15 -0600

    adding crude tests

--- runner.py
+++ runner.py
@@ -3,10 +3,16 @@ This runner when invoked will call code_generator.py and
 get the user their current 
 
 """
+from tests.test_gen import main
 from code_generator import gencode 
 
 if __name__ == "__main__":
     ns = gencode()
     type(ns)
     print("the 4 digit number is: ", ns)
+
+def runtests():
+    main()
+    print('Tests ran successfully')
+
 
--- tests/test_gen.py
+++ tests/test_gen.py
@@ -1,8 +1,11 @@
-from ..code_generator import *
+from code_generator import *
 
 def main():
     assert(DateSum().ret() <= 142)
     assert(TimeSum().ret() <= 144)
     assert(sum() <= 45)
+    assert(DateSum().ret() >= 0)
+    assert(TimeSum().ret() >= 0)
+    assert(sum() >= 0)
     
     
