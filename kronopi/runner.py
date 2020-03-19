53c2a96491301886f12e0ce9a0b75c3880f5d8ce
Luciano Remes <cybergenik@gmail.com>
2020-03-19 16:00:07 -0600

    import changes

--- kronopi/runner.py
+++ kronopi/runner.py
@@ -3,16 +3,11 @@ This runner when invoked will call code_generator.py and
 get the user their current 
 
 """
-from tests.test_gen import main
 from code_generator import gencode 
 
 if __name__ == "__main__":
     ns = gencode()
     type(ns)
     print("the 4 digit number is: ", ns)
-
-def runtests():
-    main()
-    print('Tests ran successfully')
 
 
