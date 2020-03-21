3ee812966649035a55d8d62d204b4b931501e681
Luciano Remes <cybergenik@gmail.com>
2020-03-21 12:05:49 -0600

    fix import

--- kronopi/runner.py
+++ kronopi/runner.py
@@ -3,7 +3,7 @@ This runner when invoked will call code_generator.py and
 get the user their current 
 
 """
-from code_generator import gencode
+from .code_generator import gencode
 
 if __name__ == "__main__":
     ns = gencode()
