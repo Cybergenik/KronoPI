469304726113506cb1a5e9ed53119ca11bd524c4
Luciano Remes <cybergenik@gmail.com>
2020-03-21 12:05:31 -0600

    fix import

--- kronopi/runner.py
+++ kronopi/runner.py
@@ -3,11 +3,9 @@ This runner when invoked will call code_generator.py and
 get the user their current 
 
 """
-from .code_generator import gencode 
+from code_generator import gencode
 
 if __name__ == "__main__":
     ns = gencode()
     type(ns)
     print("the 4 digit number is: ", ns)
-
-
