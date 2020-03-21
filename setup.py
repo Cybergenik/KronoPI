db9c33db8c096d81e1595c39e875cdc722765669
Luciano Remes <cybergenik@gmail.com>
2020-03-21 17:35:18 -0600

    optimizing

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="KronoPI_cybergenik", # Replace with your own username
+    name="KronoPI_cybergenik",
     version="0.0.1",
     author="Luciano Remes",
     author_email="cybergenik@gmail.com",
